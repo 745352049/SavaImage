0fbfcfb876ebaf98f69a1b361646b1cacfd10189
Baron_Nash <745352049@qq.com>
2018-11-26 12:42:38 +0800

    添加

--- 将图片保存到指定相册/ViewController.m
+++ 将图片保存到指定相册/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <Photos/Photos.h>
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *saveImageView;
 
 @end
 
@@ -17,6 +20,43 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSData *imgData = [NSData dataWithContentsOfURL:[NSURL URLWithString:@"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1543211438829&di=5feaddd38b0d0783aaa30e6b0d121ed3&imgtype=0&src=http%3A%2F%2Flife.southmoney.com%2Ftuwen%2FUploadFiles_6871%2F201805%2F20180510084854414.jpg"]];
+    self.saveImageView.image = [UIImage imageWithData:imgData];
+}
+- (IBAction)saveImageAction:(UIButton *)sender {
+    PHAssetCollection *collection = [self createCollectionWithAlbumName:@"家史馆"];
+    if (collection == nil) return;
+    NSError *error = nil;
+    [[PHPhotoLibrary sharedPhotoLibrary] performChangesAndWait:^{
+        PHAssetCollectionChangeRequest *assetCollectionChangeRequest = [PHAssetCollectionChangeRequest changeRequestForAssetCollection:collection];
+        PHAssetChangeRequest *assetChangeRequest = [PHAssetChangeRequest creationRequestForAssetFromImage:self.saveImageView.image];
+        PHObjectPlaceholder *placeholder = [assetChangeRequest placeholderForCreatedAsset];
+        [assetCollectionChangeRequest addAssets:@[placeholder]];
+    } error:&error];
+    if (error) {
+        NSLog(@"保存失败：%@", error);
+    } else {
+        NSLog(@"保存成功");
+    }
+}
+- (PHAssetCollection *)createCollectionWithAlbumName:(NSString *)albumName {
+    // 获得所有的自定义相册
+    PHFetchResult<PHAssetCollection *> *collections = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeAlbum subtype:PHAssetCollectionSubtypeAlbumRegular options:nil];
+    for (PHAssetCollection *collection in collections) {
+        if ([collection.localizedTitle isEqualToString:albumName]) {
+            return collection;
+        }
+    }
+    // 代码执行到这里，说明还没有自定义相册
+    __block NSString *createdCollectionId = nil;
+    // 创建一个新的相册
+    [[PHPhotoLibrary sharedPhotoLibrary] performChangesAndWait:^{
+        createdCollectionId = [PHAssetCollectionChangeRequest creationRequestForAssetCollectionWithTitle:albumName].placeholderForCreatedAssetCollection.localIdentifier;
+    } error:nil];
+    if (createdCollectionId == nil) return nil;
+    // 创建完毕后再取出相册
+    return [PHAssetCollection fetchAssetCollectionsWithLocalIdentifiers:@[createdCollectionId] options:nil].firstObject;
 }
 
 
